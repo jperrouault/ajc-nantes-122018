638a2006dc1f93498b03ad8a50bcf89807786293
Jeremy <jeremy.perrouault@ascadis.fr>
2018-12-23 19:12:56 +0100

    DAO

---
+++ surfy-dao/src/main/java/fr/formation/dao/IDAOFournisseur.java
@@ -0,0 +1,7 @@
+package fr.formation.dao;
+
+import fr.formation.model.Fournisseur;
+
+public interface IDAOFournisseur extends IDAO<Fournisseur> {
+
+}
---
+++ surfy-dao/src/main/java/fr/formation/dao/sql/DAOFournisseurSQL.java
@@ -0,0 +1,40 @@
+package fr.formation.dao.sql;
+
+import java.util.List;
+
+import fr.formation.dao.IDAOFournisseur;
+import fr.formation.model.Fournisseur;
+
+public class DAOFournisseurSQL implements IDAOFournisseur {
+
+	@Override
+	public List<Fournisseur> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Fournisseur findById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Fournisseur save(Fournisseur entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(Fournisseur entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(int id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
